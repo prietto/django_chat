15111701fa435d4ccd0f60405522c82de16924fb
Luis Prieto <lprieto@opalo.com.co>
2021-05-03 13:29:35 -0500

    .

--- asgi.py
+++ asgi.py
@@ -1,12 +1,13 @@
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 #from channels.routing import ProtocolTypeRouter, URLRouter
 #from django.core.asgi import get_asgi_application
 #from channels.auth import AuthMiddlewareStack
-import chat.routing
-import channels.asgi
+#import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
+from channels.asgi import get_channel_layer
+
+
 
 # application = ProtocolTypeRouter({
 #     "http": get_asgi_application(),
@@ -18,5 +19,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 #     # Just HTTP for now. (We can add other protocols later.)
 # })
 
-channel_layer = channels.asgi.get_channel_layer()
+channel_layer = get_channel_layer()
 
